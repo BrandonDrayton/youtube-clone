d097b22da7339b7f87397fd0295c79309600124f
Brandon Drayton <brandondrayton@gmail.com>
2022-09-19 13:24:40 -0400

    added axios fetch still working on bugs

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
+import { fetchFromApi } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${"selectedcategory"}`);
+  }, []);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -20,6 +24,17 @@ const Feed = () => {
         >
           Copyright 2022 Infinite Media
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: "2" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "F31503" }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
