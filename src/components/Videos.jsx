ab47062917b60c840427215e412bed591ab65c9e
Brandon Drayton <brandondrayton@gmail.com>
2022-09-26 14:51:15 -0400

    created cards for videos added demo data

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Stack, Box } from "@mui/material";
+import ChannelCard from "./ChannelCard";
+import VideoCard from "./VideoCard";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap="2">
+      {videos.map((video, index) => (
+        <Box key={index}>
+          {video.id.videoId && <VideoCard video={video} />}
+          {video.id.channelId && <ChannelCard channelDetail={video} />}
+        </Box>
+      ))}{" "}
+    </Stack>
+  );
 };
 
 export default Videos;
